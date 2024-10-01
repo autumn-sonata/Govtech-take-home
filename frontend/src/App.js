1253900e5426ce53a40ce396f6ed5037458412c2
Eric Lee Ying Yao <ericleeyy.78@gmail.com>
2024-10-01 22:00:53 +0800

    refactor: routing to homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,13 @@
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
-    <div className="App h-screen flex flex-col justify-between">
-      <header className="bg-green-500 text-white text-4xl text-center py-4">
-        We Are The Champions
-      </header>
-      <main className="flex-grow flex items-center justify-center">
-        <p className="text-xl">
-          Welcome to the landing page! 
-          <br />
-        </p>
-      </main>
-      <footer className="bg-gray-200 text-center py-2">
-      </footer>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,21 @@
+import '../styles/HomePage.css';
+
+function HomePage() {
+  return (
+    <div className="App h-screen flex flex-col justify-between">
+      <header className="bg-green-500 text-white text-4xl text-center py-4">
+        We Are The Champions
+      </header>
+      <main className="flex-grow flex items-center justify-center">
+        <p className="text-xl">
+          Welcome to the landing page! 
+          <br />
+        </p>
+      </main>
+      <footer className="bg-gray-200 text-center py-2">
+      </footer>
+    </div>
+  );
+}
+
+export default HomePage;
