3ec7b284eb8361bdd25782bff08526559b06e617
Eric Lee Ying Yao <ericleeyy.78@gmail.com>
2024-10-01 21:33:44 +0800

    feature: landing page for the project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,20 +2,18 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="App h-screen flex flex-col justify-between">
+      <header className="bg-green-500 text-white text-4xl text-center py-4">
+        We Are The Champions
       </header>
+      <main className="flex-grow flex items-center justify-center">
+        <p className="text-xl">
+          Welcome to the landing page! 
+          <br />
+        </p>
+      </main>
+      <footer className="bg-gray-200 text-center py-2">
+      </footer>
     </div>
   );
 }
