5f231cf3f2361b714cdde8f056b51bafb207ed6f
Eric Lee Ying Yao <ericleeyy.78@gmail.com>
2024-10-02 23:21:44 +0800

    refactor: Add and edit into a single component

---
+++ frontend/src/components/AddEditTeam.js
@@ -0,0 +1,94 @@
+import React, { useState } from "react";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { TextField, Button, Typography, Paper, Grid } from "@mui/material";
+
+function AddEditTeam() {
+  const [teamsInfo, setTeamsInfo] = useState("");
+
+  const handleAddTeams = async (e) => {
+    e.preventDefault();
+    toast.success("Teams added successfully!");
+    try {
+      const { data } = await axios.post("/api/teams/", {
+        teamsInfo,
+      });
+      if (data?.success) {
+        toast.success("Teams added successfully!");
+      } else {
+        toast.error(`${data.message}`);
+      }
+    } catch (error) {
+      toast.error("Something wrong occurred for input");
+    }
+  };
+
+  const handleEditTeams = async (e) => {
+    e.preventDefault();
+    toast.success("Teams added successfully!");
+    try {
+      const { data } = await axios.put("/api/teams/", {
+        teamsInfo,
+      });
+      if (data?.success) {
+        toast.success("Teams added successfully!");
+      } else {
+        toast.error(`${data.message}`);
+      }
+    } catch (error) {
+      toast.error("Something wrong occurred for input");
+    }
+  };
+
+  return (
+    <Paper
+      className="add-team p-4 shadow rounded"
+      elevation={3}
+      sx={{
+        width: "75%",
+        margin: "0 auto",
+        maxWidth: "600px",
+      }}
+    >
+      <Typography variant="h5" component="h2" className="font-bold mb-2">
+        Add or Edit Teams
+      </Typography>
+      <form>
+        <TextField
+          label="Teams information"
+          variant="outlined"
+          fullWidth
+          margin="normal"
+          value={teamsInfo}
+          onChange={(e) => setTeamsInfo(e.target.value)}
+        />
+        <Grid container spacing={2}>
+          <Grid item>
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              className="mt-2"
+              onClick={(e) => handleAddTeams(e, 'add')}
+            >
+              Add Teams
+            </Button>
+          </Grid>
+          <Grid item>
+            <Button
+              type="submit"
+              variant="contained"
+              color="secondary"
+              className="mt-2"
+              onClick={(e) => handleEditTeams(e, 'edit')}
+            >
+              Edit Teams
+            </Button>
+          </Grid>
+        </Grid>
+      </form>
+    </Paper>
+  );
+}
+
+export default AddEditTeam;
--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,5 +1,5 @@
 import TopMargin from '../components/TopMargin.js';
-import AddTeam from '../components/AddTeam';
+import AddEditTeam from '../components/AddEditTeam.js';
 import BottomMargin from '../components/BottomMargin.js';
 import '../styles/HomePage.css';
 
@@ -8,7 +8,7 @@ function HomePage() {
     <div className="App h-screen flex flex-col justify-between">
       <TopMargin />
       <main className="flex-grow flex items-center justify-center">
-        <AddTeam />
+        <AddEditTeam />
       </main>
       <BottomMargin />
     </div>
