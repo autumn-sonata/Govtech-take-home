be18a6a6748306448f8b284a2543cf70a0e15bde
Eric Lee Ying Yao <ericleeyy.78@gmail.com>
2024-10-03 02:36:16 +0800

    fix: toast not popping up issue

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import { Routes, Route } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <>
+      <Toaster position="top-right" />
       <Routes>
         <Route path="/" element={<HomePage />} />
       </Routes>
