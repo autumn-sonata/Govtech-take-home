177e0193ca43e7744903de86625427e21f72fd17
Eric Lee Ying Yao <ericleeyy.78@gmail.com>
2024-10-03 00:37:13 +0800

    fix: redundant package configurations

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import morgan from "morgan";
 import dotenv from "dotenv";
 import teamRoutes from "./routes/teamRoutes.js";
+import matchRoutes from "./routes/matchRoutes.js";
 import connectDataBase from "./db/db.js";
 
 const app = express();
